6f19dc08c3ebc2a009032486a1a476c84fc50a1a
thewolfgames <thewolfgames@yahoo.com>
2021-08-24 20:19:14 +0300

    ps4t

--- 755/jb.js
+++ 755/jb.js
@@ -142974,3 +142974,9 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
+if(main_ret == 179 || main_ret == 0){
+	document.getElementById("progress").innerHTML="Jailbreak Complete.. Now Run Payloads!!";
+} else{
+	alert("Jailbreak Failed!! Please Restart and Start Fresh!!!");
+	document.getElementById("progress").innerHTML="Jailbreak Failed, Restart and Start Fresh !!";
+}
